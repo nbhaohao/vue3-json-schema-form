dca8b16c98a4de46f49942c195be23e95f7caf5a
nbhaohao <nbhaohao@163.com>
2021-03-28 13:12:09 +0800

    feat:完成后续 examples 使用的代码编辑器

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
-import { defineComponent } from "vue";
+import { defineComponent, ref, Ref } from "vue";
+import { createUseStyles } from "vue-jss";
+import MonacoEditor from "@/components/MonacoEditor";
+
+function toJson(data: any) {
+  return JSON.stringify(data, null, 2);
+}
+
+const schema = {
+  type: "string",
+};
+
+const useStyles = createUseStyles({
+  editor: {
+    minHeight: 400,
+  },
+});
 
 export default defineComponent({
-  render() {
-    return <div>hello world</div>;
+  setup() {
+    const schemaRef: Ref = ref(schema);
+    const handleCodeChange = (code: string) => {
+      let schema: any;
+      try {
+        schema = JSON.parse(code);
+        schemaRef.value = schema;
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    const classesRef = useStyles();
+    return () => {
+      const classes = classesRef.value;
+      const code = toJson(schemaRef.value);
+      return (
+        <div>
+          <MonacoEditor
+            class={classes.editor}
+            title="Schema"
+            onChange={handleCodeChange}
+            code={code}
+          />
+        </div>
+      );
+    };
   },
 });
