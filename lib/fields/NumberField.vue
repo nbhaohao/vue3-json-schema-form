eec646f4d7c42b21f73c496216e64d9c9efd8d8a
nbhaohao <nbhaohao@163.com>
2021-03-28 19:56:07 +0800

    feat:完成 NumberField 组件

---
+++ lib/fields/NumberField.vue
@@ -0,0 +1,20 @@
+<template>
+  <input type="number" :value="value" @input="handleChange" />
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+import { FieldPropsDefine } from "../types";
+
+const props = defineProps({ ...FieldPropsDefine });
+
+const handleChange = (e: InputEvent) => {
+  const input = (e.target as HTMLInputElement).value;
+  const num = Number(input);
+  if (Number.isNaN(num)) {
+    props.onChange(undefined);
+    return;
+  }
+  props.onChange(num);
+};
+</script>
