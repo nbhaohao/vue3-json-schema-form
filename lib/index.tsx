b3fd95ba8ef66058ebf36d1e315cb66eafdb3fcc
nbhaohao <nbhaohao@163.com>
2021-04-01 20:57:37 +0800

    test:尝试 NumberField 单元测试

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,3 +1,5 @@
 import SchemaForm from "./SchemaForm";
 
+export { default as NumberField } from "./fields/NumberField.vue";
+
 export default SchemaForm;
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,26 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { mount } from "@vue/test-utils";
+import SchemaForm from "../../lib/SchemaForm";
+import { NumberField } from "../../lib";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
+describe("SchemaForm", () => {
+  it("should render correct number field", async () => {
+    let value = "";
+    const wrapper = mount(SchemaForm, {
+      props: {
+        schema: {
+          type: "number",
+        },
+        value: "",
+        onChange: (v: any) => {
+          value = v;
+        },
+      },
     });
-    expect(wrapper.text()).toMatch(msg);
+    const numberField = wrapper.findComponent(NumberField);
+    expect(numberField.exists()).toBeTruthy();
+    const input = numberField.find("input");
+    input.element.value = "123";
+    await input.trigger("input");
+    expect(value).toBe(123);
   });
 });
